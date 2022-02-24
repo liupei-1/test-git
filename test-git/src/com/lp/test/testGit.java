8a60317747c4ecf6d01d491ee0610ae4f1d6c4c4
itLiu <2200753679@qq.com>
2022-02-24 21:30:38 +0800

    修改代码

--- test-git/src/com/lp/test/testGit.java
+++ test-git/src/com/lp/test/testGit.java
@@ -7,5 +7,7 @@ package com.lp.test;
 public class testGit {
     public static void main(String[] args) {
         System.out.println("hello git");
+        System.out.println("hello git");
+        System.out.println("hello git");
     }
 }
