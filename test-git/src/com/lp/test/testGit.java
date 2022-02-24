41aa5dbb220828bf22437067c1bfbe77a35ffddf
itLiu <2200753679@qq.com>
2022-02-24 18:33:53 +0800

    添加test-git到git里面

---
+++ test-git/src/com/lp/test/testGit.java
@@ -0,0 +1,11 @@
+package com.lp.test;
+
+/**
+ * @author LiuPei
+ * @date 2022/2/24 18:31
+ */
+public class testGit {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+}
